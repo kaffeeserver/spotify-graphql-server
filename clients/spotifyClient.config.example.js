f77a3f0eabedce83bf8c83da4abe6bce6ca05be9
Christoph Glaß <mail.kaffeeserver@googlemail.com>
2017-08-10 15:15:08 +0200

    spotify client that gets his own access_token (since nodejs module caching it acts as singleton)

---
+++ clients/spotifyClient.config.example.js
@@ -0,0 +1,10 @@
+/**
+ * 1. register your app under 'https://developer.spotify.com/my-applications'
+ * 2. put your own clientId and clientSecret in here
+ * 3. rename this file to 'spotifyClient.config.js'
+ */
+module.exports = {
+    clientId: 'yourClientId',
+    clientSecret: 'yourClientSecret',
+    redirectUri: '/'
+};
