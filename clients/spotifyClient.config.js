593dd54c0397e196e03d41faccb0a54205a5632f
Christoph Glaß <mail.kaffeeserver@googlemail.com>
2017-08-10 22:54:14 +0200

    new secret

--- clients/spotifyClient.config.js
+++ clients/spotifyClient.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     clientId: 'd69539fe70264c5a881bfc1c01ad8d30',
-    clientSecret: '573ab43f16524523bc21a3405a3801e6',
+    clientSecret: '127b8123dd04483ebaeadfe47c7367fb',
     redirectUri: '/'
 };
