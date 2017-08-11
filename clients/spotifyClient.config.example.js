b06514f59a2023bacb10df90337202bcd6611178
Christoph Glaß <mail.kaffeeserver@googlemail.com>
2017-08-11 14:12:08 +0200

    heroku merge conflicts

--- clients/spotifyClient.config.example.js
+++ clients/spotifyClient.config.example.js
@@ -1,21 +1,11 @@
 /**
-<<<<<<< HEAD
  * 1. register your app under https://developer.spotify.com/my-applications
  * 2. put your own clientId and clientSecret in here
  * 3. rename this file to 'spotifyClient.config.js'
  * 4. Also see spotifyClient.config.heroku.js for deploying your app on https://dashboard.heroku.com/
-=======
- * 1. register your app under 'https://developer.spotify.com/my-applications'
- * 2. put your own clientId and clientSecret in here
- * 3. rename this file to 'spotifyClient.config.js'
->>>>>>> 593dd54c0397e196e03d41faccb0a54205a5632f
  */
 module.exports = {
     clientId: 'yourClientId',
     clientSecret: 'yourClientSecret',
-<<<<<<< HEAD
     redirectUri: 'yourRedirectUrl'
-=======
-    redirectUri: '/'
->>>>>>> 593dd54c0397e196e03d41faccb0a54205a5632f
 };
